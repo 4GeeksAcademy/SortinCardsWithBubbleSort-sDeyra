f992d42939c31c1e0009caf8e51ca0de7a34bfae
sDeyra <sdeymt@gmail.com>
2025-08-16 02:26:10 +0000

    comenrando cosas solamente del generador de cartas

--- src/app.js
+++ src/app.js
@@ -7,5 +7,33 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  generarCarta()
+  document.getElementById("botonSort").addEventListener("click", generarCarta)
+  
 };
+
+
+function generarCarta() {
+
+  let palo = ["♦", "♥", "♠", "♣"]
+  let numero = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+
+  let paloAleatorio = palo[Math.floor(Math.random() * palo.length)];
+  console.log(paloAleatorio)
+
+  let numeroAleatorio = numero[Math.floor(Math.random() * numero.length)];
+  console.log(numeroAleatorio)
+
+  document.getElementById("centro").innerHTML = numeroAleatorio
+  document.getElementById("arriba").innerHTML = paloAleatorio
+  document.getElementById("abajo").innerHTML = paloAleatorio
+
+  if (paloAleatorio == "♥" || paloAleatorio == "♦") {
+  document.getElementById("arriba").style.color = "red";
+  document.getElementById("abajo").style.color = "red";
+} else {
+  document.getElementById("arriba").style.color = "black";
+  document.getElementById("abajo").style.color = "black";
+}
+}
+
