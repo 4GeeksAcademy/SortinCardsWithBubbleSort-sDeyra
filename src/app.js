1326737c798181c7191596cb9db45b53db9ca1c4
sDeyra <sdeymt@gmail.com>
2025-08-18 07:06:02 +0000

    generar varias cartas

--- src/app.js
+++ src/app.js
@@ -5,18 +5,27 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = function () {
   //write your code here
-  generarCarta()
-  document.getElementById("botonSort").addEventListener("click", generarCarta)
-  
+  document.getElementById("botonSort").addEventListener("click", () =>{
+    const cantidad = parseInt(document.getElementById("inputCantidad").value);
+
+    if (isNaN(cantidad) || cantidad < 1 || cantidad > 52){
+      alert("porfavor introduce un numero valido entre 1 y 52");
+      return;
+    }
+
+    variasCartasAleatorias(cantidad);
+  })
+
 };
 
+let palo = ["♦", "♥", "♠", "♣"]
+let numero = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
 
-function generarCarta() {
 
-  let palo = ["♦", "♥", "♠", "♣"]
-  let numero = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+
+const generarCarta = () => {
 
   let paloAleatorio = palo[Math.floor(Math.random() * palo.length)];
   console.log(paloAleatorio)
@@ -24,16 +33,36 @@ function generarCarta() {
   let numeroAleatorio = numero[Math.floor(Math.random() * numero.length)];
   console.log(numeroAleatorio)
 
-  document.getElementById("centro").innerHTML = numeroAleatorio
-  document.getElementById("arriba").innerHTML = paloAleatorio
-  document.getElementById("abajo").innerHTML = paloAleatorio
+  return {palo : paloAleatorio, valor: numeroAleatorio};
+}
 
-  if (paloAleatorio == "♥" || paloAleatorio == "♦") {
-  document.getElementById("arriba").style.color = "red";
-  document.getElementById("abajo").style.color = "red";
-} else {
-  document.getElementById("arriba").style.color = "black";
-  document.getElementById("abajo").style.color = "black";
+const crearCartaHTML = (carta) => {
+  let divCarta = document.createElement("div")
+  divCarta.className = "carta"
+
+  let color = (carta.palo == "♥" || carta.palo == "♦") ? "red" : "black";
+
+  divCarta.innerHTML = `
+  <div style="color: ${color}; font-size: 24px;">${carta.palo}</div>
+  <div style="font-size: 32px;">${carta.valor}</div>
+  <div style="color: ${color}; font-size: 24px;">${carta.palo}</div>
+`;
+  return divCarta;
 }
+
+const variasCartasAleatorias = (cantidad) => {
+  if (cantidad < 1) {
+    alert("No se puede hacer pai");
+    return
+  };
+  const contenedor = document.getElementById("contenedorCartas1");
+  contenedor.innerHTML = "";
+
+  for (let i = 0; i < cantidad; i++) {
+    let carta = generarCarta();
+    let cartaHTML = crearCartaHTML(carta)
+    contenedor.appendChild(cartaHTML)
+  }
 }
+
 
